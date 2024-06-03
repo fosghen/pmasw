9db501741369117d8397afdce657fe0bf7f48ad4
fosghen <r.efremov2@g.nsu.ru>
2024-06-03 13:52:58 +0700

    Дополнен необходимый функционал

--- DataReader.py
+++ DataReader.py
@@ -5,9 +5,15 @@
     segy, baykal.
 """
 
+from pathlib import Path
+from struct import unpack
+from warnings import catch_warnings, simplefilter
+
 import numpy as np
 from segyio import dt as dt_
 from segyio import open as open_
+
+from PassiveData import PassiveData
 
 
 class DataLoader:
@@ -185,11 +191,44 @@ class DataLoader:
 
 
     @staticmethod
+    def _open_check_warn(path):
+        """
+        Перехватывает предупреждение.
+
+        Перехватывание предупреждения о том, что endian неверен и
+        открываем с другим endian.
+
+        Parameters
+        ----------
+        path : srt
+            Путь до файла.
+
+        Returns
+        -------
+        segy_file : segyio.SegyFile
+            Открытый segy-файл.
+
+        """
+        with catch_warnings(record=True) as w:
+            simplefilter("always")
+            try:
+                segy_file = open_(path, ignore_geometry=True, endian='big')
+            except Exception as e: # Выяснить какой тип ошибки соответсвует
+                                   # неверному endian
+                print(f"Исключение: {e}")
+                segy_file = open_(path, ignore_geometry=True, endian='little')
+            if w:
+                segy_file = open_(path, ignore_geometry=True, endian='little')
+        return segy_file
+
+
+    @staticmethod
     def read_segy(path):
         """
         Читает файлы формата segy.
 
-        Чтение файлов формата segy, используется библиотека segyio
+        Чтение файлов формата segy с использованием
+        библиотеки segyio.
 
         Parameters
         ----------
@@ -200,16 +239,119 @@ class DataLoader:
         -------
         data : ndarray[dtype: float64, dim = 2]
             Двумерный массив данных.
+            [tempor_axis, spatial_axis]
 
         dt : float
             Время дексретизации в секундах.
 
         """
-        try:
-            segy_file = open_(path, ignore_geometry=True, endian='big')
-        except: # Выяснить какой тип ошибки соответсвует неверному endian
-            segy_file = open_(path, ignore_geometry=True, endian='little')
+        segy_file = DataLoader._open_check_warn(path)
         data = np.array([np.copy(tr) for tr in segy_file.trace[:]],
                         dtype=np.float64).T
         dt = dt_(segy_file) * 1e-6
         return data, dt
+
+
+    @staticmethod
+    def read_baykal(path):
+        """
+        Читает файлы формата baykal.
+
+        Чтение файлов формата baykal, выполняется чтение байтов
+        файла.
+
+        Parameters
+        ----------
+        path : srt
+            Путь до файла.
+
+        Returns
+        -------
+        data : ndarray[dtype: float64, dim = 2]
+            Двумерный массив данных.
+            [tempor_axis, spatial_axis]
+
+        dt : float
+            Время дексретизации в секундах.
+
+        """
+        with Path(path).open('rb') as f:
+            channels = unpack('H', f.read(2))[0] # Колличество каналов
+            f.read(2)
+            _ = unpack('H', f.read(2))[0] # Версия
+            _ = unpack('H', f.read(2))[0] # День
+            _ = unpack('H', f.read(2))[0] # Месяц
+            _ = unpack('H', f.read(2))[0] # Год
+            f.read(6)
+            _ = unpack('H', f.read(2))[0] # Разрядность АЦП
+            f.read(2)
+            freq = unpack('H', f.read(2))[0] # Частота дискретизации
+            f.read(8)
+            station_name = unpack('9s7c', f.read(16))[0] # Название станции
+            station_name = str(station_name).replace('b', '')
+            station_name = station_name.replace("'", '')
+            f.read(24)
+            _ = unpack('d', f.read(8))[0] # Широта
+            _ = unpack('d', f.read(8))[0] # Долгота
+            f.read(16)
+            _ = unpack('Q', f.read(8))[0] # Начальное время
+            f.read(8)
+            channels_number = [] # номер канала
+            channels_name = [] # имя канала
+            sensors_type = [] # тип сенсора
+            channels_coef = [] # коэффициент канала
+            for i in range(channels):
+                # Номер канала
+                channels_number.append(unpack('H', f.read(2))[0])
+                f.read(6)
+                # Имя канала
+                channels_name.append(unpack('6i', f.read(24))[0])
+                # Тип сенсора
+                sensors_type.append(unpack('6i', f.read(24))[0])
+                # Коэффициент канала
+                channels_coef.append(unpack('d', f.read(8))[0])
+                f.read(8)
+
+            data = np.fromfile(f, dtype = 'int')
+            data = np.reshape(data, [channels, len(data) // channels],
+                              order='F')
+            dt = 1 / freq
+            return data.T, dt
+
+
+    def read_data(self):
+        """
+        Считывание сейсмических данных.
+
+        Считывание выполняется на основе иницализированных
+        параметров: формата, количества компонент и компоненты.
+
+        Returns
+        -------
+        data : PassiveData
+            Сейсмические данные.
+
+        """
+        if self._format == "segy":
+            try:
+                data, dt = DataLoader.read_segy(self._path)
+            except RuntimeError:
+                raise ValueError("Неверный тип данных")
+
+        if self._format == "baykal":
+            try:
+                data, dt = DataLoader.read_baykal(self._path)
+            except ValueError:
+                raise ValueError("Неверный тип данных")
+
+        if self._n_components == '3C':
+            if self._component == 'Z':
+                data = data[:,0::3]
+            elif self._component == 'X':
+                data = data[:,1::3]
+            elif self._component == 'Y':
+                data = data[:,3::3]
+
+        return PassiveData(data = data,
+                           dt = dt)
+
